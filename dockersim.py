4cdc8dc5007d8a2d4f64c9a1c06e8809fae06e13
Matthias Blochberger <matthias.blochberger@esat.kuleuven.be>
2023-07-20 16:24:34 +0200

    dockersim now runs somewhat

--- dockersim.py
+++ dockersim.py
@@ -12,50 +12,54 @@ class DockerSim:
     variable_names: list[str]
     seed: int
 
-    def __init__(self, func: Callable, tasks: Iterable[dict], seed: int) -> None:
+    def __init__(
+        self, func: Callable, tasks: Iterable[dict], seed: int, datadir: str = "."
+    ) -> None:
         self.func = func
         self.tasks = tasks
-        self.variable_names = tasks[0].keys()
-        print(self.variable_names)
+        self.variable_names = list(tasks[0].keys())
         self.seed = seed
+        self.datadir = datadir
         pass
 
     def _workerProc(self, data):
-        file_name = f"data/results_{mp.current_process().name}.csv"
-        result_file = open(file_name, mode="a+")
-        result_writer = csv.writer(
-            result_file,
-            delimiter=",",
-            quotechar='"',
-            quoting=csv.QUOTE_MINIMAL,
-        )
-        if os.stat(file_name).st_size == 0:
-            result_writer.writerow(["run_nr", "a", "b", "series", "value"])
+        file_name = f"{self.datadir}/results_{mp.current_process().name}.csv"
+        with open(file_name, mode="a+") as result_file:
+            result_writer = csv.DictWriter(
+                result_file,
+                fieldnames=["run_nr", *self.variable_names, "series", "value"],
+                delimiter=",",
+                quotechar='"',
+                quoting=csv.QUOTE_MINIMAL,
+            )
+            if os.stat(file_name).st_size == 0:
+                result_writer.writeheader()
 
-        run_nr = data["run_nr"]
-        rng = np.random.default_rng(np.random.PCG64DXSM(self.seed).jumped(run_nr))
-        del data["run_nr"]
-        result = self.func(rng=rng, **data)
-        result_writer.writerows(
-            [
-                [run_nr, data["a"], data["b"], series, value]
+            run_nr = data["run_nr"]
+            rng = np.random.default_rng(np.random.PCG64DXSM(self.seed).jumped(run_nr))
+            del data["run_nr"]
+            result = self.func(rng=rng, **data)
+            result_writer.writerows(
+                {"run_nr": run_nr, **data, "series": series, "value": value}
                 for series, value in enumerate(result)
-            ]
-        )
-        result_file.close()
+            )
 
     def run(
         self,
+        runs: int = 50,
         num_processes: int = 4,
         seed: int = None,
     ):
         self.seed = self.seed if seed is None else seed
+        run_tasks = []
+        for task in self.tasks:
+            run_tasks += [{"run_nr": run, **task} for run in range(runs)]
         with mp.Pool(processes=num_processes) as pool:
             with tqdm(
-                total=len(self.tasks),
+                total=len(run_tasks),
                 position=0,
                 leave=True,
                 unit="runs",
             ) as pbar:
-                for _ in pool.imap(self._workerProc, self.tasks):
+                for _ in pool.imap(self._workerProc, run_tasks):
                     pbar.update()
